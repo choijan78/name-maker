f305c215046fc67d4c88b18735e9f65a1ae852b6
choijan78 <choijan78@gmail.com>
2023-06-20 23:15:23 +0900

    use pug template

--- src/controllers/namingController.js
+++ src/controllers/namingController.js
@@ -1,4 +1,4 @@
-export const home = (req, res) => res.send("Home");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
 export const pet = (req, res) => res.send("Pet");
 export const baby = (req, res) => res.send("Baby");
 export const alias = (req, res) => res.send("Nickname");
